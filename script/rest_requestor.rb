63bea5ed495a17ff7a1b0814ed4ef46e9f0412bd
daashandayo <dj30c.1@gmail.com>
2019-05-02 17:44:56 +0530

    Project complete

--- script/rest_requestor.rb
+++ script/rest_requestor.rb
@@ -1,4 +1,18 @@
 require 'rest-client'
-url = "http://localhost:3000/users/new"
+# => INDEX
+url = "http://localhost:3000/users"
 
+# => SHOW
+# url = "http://localhost:3000/users/1"
+
+# => NEW
+# url = "http://localhost:3000/users/new"
+
+# => CREATE
+# posturl = "http://localhost:3000/users/"
+
+# GET REQUESTS
 puts RestClient.get(url)
+
+# POST REQUEST
+# puts RestClient.post(posturl,"")
